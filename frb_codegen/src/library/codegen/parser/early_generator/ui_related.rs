e067f2d48ff108cf0c589b70c3bae0affb053586
Noel Jacob <noeljacob91@gmail.com>
2024-09-21 12:06:57 +0530

    Change UI early_gen

--- frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
+++ frb_codegen/src/library/codegen/parser/early_generator/ui_related.rs
@@ -76,17 +76,6 @@ impl BaseRustState {
 
 fn generate_dart_boilerplate() -> String {
     r###"
-import 'package:flutter/material.dart';
-
-Future<void> runRustApp({
-  required Widget Function(RustState state) body,
-  required RustState Function() state,
-}) async {
-  await RustLib.init();
-  runApp(_MyApp(body: body, state: state()));
-}
-
-// improve typing later
 class _MyApp extends StatefulWidget {
   final Widget Function(RustState state) body;
   final RustState state;
@@ -128,62 +117,6 @@ class _MyAppState extends State<_MyApp> {
         useMaterial3: true,
       ),
       home: Scaffold(body: widget.body(widget.state)),
-    );
-  }
-}
-
-/// Adapted from https://github.com/mobxjs/mobx.dart/issues/750
-// TODO: Move to support library instead of inlining here
-class SyncTextField extends StatefulWidget {
-  final String text;
-
-  // forward
-  final ValueChanged<String>? onChanged;
-  final InputDecoration? decoration;
-  final ValueChanged<String>? onSubmitted;
-
-  const SyncTextField({
-    super.key,
-    required this.text,
-    this.onChanged,
-    this.decoration,
-    this.onSubmitted,
-  });
-
-  @override
-  State<SyncTextField> createState() => _SyncTextFieldState();
-}
-
-class _SyncTextFieldState extends State<SyncTextField> {
-  late final TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-    _controller.text = widget.text;
-  }
-
-  @override
-  void didUpdateWidget(covariant SyncTextField oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.text != widget.text) _controller.text = widget.text;
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: _controller,
-      // forward
-      onChanged: widget.onChanged,
-      decoration: widget.decoration,
-      onSubmitted: widget.onSubmitted,
     );
   }
 }
